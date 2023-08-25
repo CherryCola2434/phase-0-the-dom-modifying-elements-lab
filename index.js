8e77ac05fdb132c33129495fa273daf5586fe878
Cynthia Lopez <sleepyroad2005@gmail.com>
2023-08-25 18:29:53 -0500

    finally

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const newHeader = document.createElement("h1");
+const main = document.getElementById("main")
+main.remove();
+newHeader.id = "victory"
+newHeader.textContent = "YOUR-NAME is the champion";
